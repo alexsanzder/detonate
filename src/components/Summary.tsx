ef6dd00724870f3e48c3a03ba3450a785400bcaa
Alex Sanz <alex.sanzb@gmail.com>
2020-02-19 23:44:42 +0100

    Render list data from google sheets :+1:

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Content,
   FlexboxGrid,
@@ -14,9 +14,11 @@ import {
   IconStack,
   Icon,
   TagGroup,
-  Tag
-} from "rsuite";
-import styled from "styled-components";
+  Tag,
+} from 'rsuite';
+import styled from 'styled-components';
+
+import GoogleAuthContext from './../contexts/useGoogleAuth';
 
 const Style = styled.div`
    {
@@ -28,52 +30,82 @@ const { Paragraph } = Placeholder;
 
 const PanelHeader = () => {
   return (
-    <FlexboxGrid justify="space-between">
+    <FlexboxGrid justify='space-between'>
       <FlexboxGrid.Item colspan={20}>Tue, 18 Feb</FlexboxGrid.Item>
-      <FlexboxGrid.Item colspan={3} style={{ textAlign: "right" }}>
+      <FlexboxGrid.Item
+        colspan={3}
+        style={{
+          textAlign: 'right',
+        }}
+      >
         23:20:56
       </FlexboxGrid.Item>
     </FlexboxGrid>
   );
 };
 const Summary = () => {
+  const { records } = React.useContext(GoogleAuthContext);
   return (
     <Style>
       <Panel header={<PanelHeader />} bordered>
         <List hover>
-          <List.Item>
-            <FlexboxGrid justify="space-between" align="middle">
-              <FlexboxGrid.Item colspan={20}>
-                <p
+          {records ? (
+            records.map((record, idx) => (
+              <List.Item key={idx}>
+                <FlexboxGrid justify='space-between' align='middle'>
+                  <FlexboxGrid.Item colspan={20}>
+                    <p
+                      style={{
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                      }}
+                    >
+                      {/* Title */}
+                      {record[4]}
+                    </p>
+                  </FlexboxGrid.Item>
+                  <FlexboxGrid.Item
+                    colspan={4}
+                    style={{
+                      textAlign: 'right',
+                    }}
+                  >
+                    {/* Time */}
+                    {record[6]}
+                  </FlexboxGrid.Item>
+                </FlexboxGrid>
+                <FlexboxGrid
+                  justify='space-between'
+                  align='middle'
                   style={{
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis"
+                    marginTop: '1rem',
                   }}
                 >
-                  CREATEAPP-56 Change Webpack allowing sass and less loaders
-                </p>
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={4} style={{ textAlign: "right" }}>
-                00:43:10
-              </FlexboxGrid.Item>
-            </FlexboxGrid>
-            <FlexboxGrid
-              justify="space-between"
-              align="middle"
-              style={{ marginTop: "1rem" }}
-            >
-              <FlexboxGrid.Item colspan={20}>
-                <TagGroup>
-                  <Tag>CREATEAPP-60</Tag>
-                  <Tag>Facebook</Tag>
-                </TagGroup>
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={4} style={{ textAlign: "right" }}>
-                <Icon icon="play" size="lg" />
-              </FlexboxGrid.Item>
-            </FlexboxGrid>
-          </List.Item>
+                  <FlexboxGrid.Item colspan={20}>
+                    <TagGroup>
+                      {/* Ticket */}
+                      {record[5] && <Tag>{record[5]}</Tag>}
+                      {/* Project */}
+                      {record[3] && <Tag>{record[3]}</Tag>}
+                      {/* Company */}
+                      {record[2] && <Tag>{record[2]}</Tag>}
+                    </TagGroup>
+                  </FlexboxGrid.Item>
+                  <FlexboxGrid.Item
+                    colspan={4}
+                    style={{
+                      textAlign: 'right',
+                    }}
+                  >
+                    <Icon icon='play' size='lg' />
+                  </FlexboxGrid.Item>
+                </FlexboxGrid>
+              </List.Item>
+            ))
+          ) : (
+            <Paragraph />
+          )}
         </List>
       </Panel>
     </Style>

027179e5ef702ccf87e1ddda10e05d64214a3b2c
Alex Sanz <alex.sanzb@gmail.com>
2020-02-21 00:46:34 +0100

    Add groupsBy :+1:

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Content,
   FlexboxGrid,
@@ -14,31 +14,45 @@ import {
   IconStack,
   Icon,
   TagGroup,
-  Tag
-} from "rsuite";
-import styled from "styled-components";
+  Tag,
+} from 'rsuite';
+import styled from 'styled-components';
 
-import GoogleAuthContext from "./../contexts/useGoogleAuth";
+import GoogleAuthContext from './../contexts/useGoogleAuth';
 
 const Style = styled.div`
    {
-    padding: 20px;
+    padding: 0 20px;
+    .list-summary {
+      box-shadow: none;
+      .rs-list-item-lg {
+        box-shadow: none;
+      }
+      .rs-panel-body {
+        padding-bottom: 0;
+      }
+    }
   }
 `;
 
 const { Paragraph } = Placeholder;
 
-const PanelHeader = () => {
+const PanelHeader = ({ records }: any) => {
+  console.log(records);
+  const sum = records.reduce((total: number, currentValue: any) => {
+    return total + currentValue.time;
+  }, 0);
+  console.log(sum);
   return (
-    <FlexboxGrid justify="space-between">
-      <FlexboxGrid.Item colspan={20}>Tue, 18 Feb</FlexboxGrid.Item>
+    <FlexboxGrid justify='space-between'>
+      <FlexboxGrid.Item colspan={20}>{records.date}</FlexboxGrid.Item>
       <FlexboxGrid.Item
         colspan={3}
         style={{
-          textAlign: "right"
+          textAlign: 'right',
         }}
       >
-        23:20:56
+        00:00:00
       </FlexboxGrid.Item>
     </FlexboxGrid>
   );
@@ -46,64 +60,87 @@ const PanelHeader = () => {
 const Summary = () => {
   const { records } = React.useContext(GoogleAuthContext);
 
+  const timeConvert = (fraction: number) => {
+    const hours = Math.floor(fraction);
+    const allseconds = 3600 * (fraction - hours);
+    const minutes = Math.floor(allseconds / 60);
+    const seconds = Math.floor(allseconds % 60);
+
+    const formatted =
+      ('0' + (hours % 12)).substr(-2) +
+      ':' +
+      ('0' + minutes).substr(-2) +
+      ':' +
+      ('0' + seconds).substr(-2);
+
+    return formatted;
+  };
   return (
     <Style>
-      <Panel header={<PanelHeader />} bordered>
-        <List hover>
-          {records ? (
-            records.map(record => (
-              <List.Item key={record.id}>
-                <FlexboxGrid justify="space-between" align="middle">
-                  <FlexboxGrid.Item colspan={20}>
-                    <p
-                      style={{
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis"
-                      }}
-                    >
-                      {record.description}
-                    </p>
-                  </FlexboxGrid.Item>
-                  <FlexboxGrid.Item
-                    colspan={4}
-                    style={{
-                      textAlign: "right"
-                    }}
-                  >
-                    {record.time}
-                  </FlexboxGrid.Item>
-                </FlexboxGrid>
-                <FlexboxGrid
-                  justify="space-between"
-                  align="middle"
-                  style={{
-                    marginTop: "1rem"
-                  }}
-                >
-                  <FlexboxGrid.Item colspan={20}>
-                    <TagGroup>
-                      {<Tag color="blue">{record.company}</Tag>}
-                      {<Tag>{record.ticket}</Tag>}
-                      {<Tag>{record.project}</Tag>}
-                    </TagGroup>
-                  </FlexboxGrid.Item>
-                  <FlexboxGrid.Item
-                    colspan={4}
-                    style={{
-                      textAlign: "right"
-                    }}
-                  >
-                    <Icon icon="play" size="lg" />
-                  </FlexboxGrid.Item>
-                </FlexboxGrid>
-              </List.Item>
-            ))
-          ) : (
-            <Paragraph />
-          )}
-        </List>
-      </Panel>
+      <List bordered={false} className='list-summary' size='lg'>
+        {records ? (
+          Object.keys(records).map(key => (
+            <List.Item key={key}>
+              <Panel header={<PanelHeader records={records[key]} />} bordered>
+                <List hover bordered={false}>
+                  {records[key].map((record: any) => {
+                    return (
+                      <List.Item key={record.id}>
+                        <FlexboxGrid justify='space-between' align='middle'>
+                          <FlexboxGrid.Item colspan={20}>
+                            <p
+                              style={{
+                                whiteSpace: 'nowrap',
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                              }}
+                            >
+                              {record.description}
+                            </p>
+                          </FlexboxGrid.Item>
+                          <FlexboxGrid.Item
+                            colspan={4}
+                            style={{
+                              textAlign: 'right',
+                            }}
+                          >
+                            {timeConvert(record.time)}
+                          </FlexboxGrid.Item>
+                        </FlexboxGrid>
+                        <FlexboxGrid
+                          justify='space-between'
+                          align='middle'
+                          style={{
+                            marginTop: '1rem',
+                          }}
+                        >
+                          <FlexboxGrid.Item colspan={20}>
+                            <TagGroup>
+                              {<Tag color='blue'>{record.company}</Tag>}
+                              {<Tag>{record.ticket}</Tag>}
+                              {<Tag>{record.project}</Tag>}
+                            </TagGroup>
+                          </FlexboxGrid.Item>
+                          <FlexboxGrid.Item
+                            colspan={4}
+                            style={{
+                              textAlign: 'right',
+                            }}
+                          >
+                            <Icon icon='play' size='lg' />
+                          </FlexboxGrid.Item>
+                        </FlexboxGrid>
+                      </List.Item>
+                    );
+                  })}
+                </List>
+              </Panel>
+            </List.Item>
+          ))
+        ) : (
+          <Paragraph />
+        )}
+      </List>
     </Style>
   );
 };
--- src/hooks/useGoogle.ts
+++ src/hooks/useGoogle.ts
@@ -1,8 +1,8 @@
 /* global gapi */
-import { useState, useEffect } from "react";
-import { useScript } from "./useScript";
+import { useState, useEffect } from 'react';
+import { useScript } from './useScript';
 
-const GOOGLE_API_SOURCE = "https://apis.google.com/js/api.js";
+const GOOGLE_API_SOURCE = 'https://apis.google.com/js/api.js';
 
 export interface GoogleUser extends gapi.auth2.GoogleUser {
   googleId?: string;
@@ -15,7 +15,7 @@ export interface UseGoogleOptions {
   apiKey?: string | undefined;
   scope?: string | undefined;
   discoveryDocs?: string[] | undefined;
-  spreadsheetId?: string | "1Lqm0iIOp5BYMtws8B5LOEy-wRC-fhOH6aPVFb5-N7Us";
+  spreadsheetId?: string | '1Lqm0iIOp5BYMtws8B5LOEy-wRC-fhOH6aPVFb5-N7Us';
 }
 export interface UseGoogleType {
   currentUser: any | undefined;
@@ -23,8 +23,8 @@ export interface UseGoogleType {
   isInitialized: boolean;
   handleSignIn: () => Promise<void> | undefined;
   handleSignOut: () => Promise<void> | undefined;
-  projects: string[];
-  records: Record[];
+  projects: any;
+  records: any;
 }
 
 export interface Record {
@@ -38,16 +38,16 @@ export interface Record {
   time: string;
 }
 
-const tablName = "aSa";
+const tablName = 'aSa';
 
 export const useGoogle = ({
   clientId,
   apiKey,
   scope,
   discoveryDocs,
-  spreadsheetId
+  spreadsheetId,
 }: UseGoogleOptions) => {
-  const [isScriptLoaded] = useScript(GOOGLE_API_SOURCE, "gapi");
+  const [isScriptLoaded] = useScript(GOOGLE_API_SOURCE, 'gapi');
   const [GoogleAuth, setGoogleAuth] = useState();
   const [isInitialized, setInitialized] = useState(false);
   const [isSignedIn, setSignedIn] = useState(false);
@@ -59,7 +59,7 @@ export const useGoogle = ({
     return {
       id: `${tablName}!A${index + 2}`,
       company: value[0],
-      project: value[1]
+      project: value[1],
     };
   };
 
@@ -72,32 +72,34 @@ export const useGoogle = ({
       project: value[3],
       description: value[4],
       ticket: value[5],
-      time: value[6]
+      time: value[6],
     };
   };
 
+  const groupBy = (array: any, key: any) => {
+    return array.reduce((result: any, currentValue: string) => {
+      // If an array already present for key, push it to the array. Else create an array and push the object
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      return result;
+    }, {});
+  };
+
   const loadTable = async () => {
-    gapi.client.load("sheets", "v4", async () => {
+    gapi.client.load('sheets', 'v4', async () => {
       const response = await gapi.client.sheets.spreadsheets.values.batchGet({
-        spreadsheetId: "1aPo1wlEXueb6poGt7X3XjYVy-VPDaGJhOO5pNBMdl48",
-        ranges: ["projects!A2:B", `${tablName}!A2:G`]
+        spreadsheetId: '1aPo1wlEXueb6poGt7X3XjYVy-VPDaGJhOO5pNBMdl48',
+        ranges: ['projects!A2:B', `${tablName}!A2:G`],
       });
       const valueRanges = response.result.valueRanges;
       const projects =
         valueRanges && valueRanges[0].values?.map(parseProjects).reverse();
-      setProjects(projects);
+      setProjects(groupBy(projects, 'company'));
+
       const records =
         valueRanges && valueRanges[1].values?.map(parseRecords).reverse();
-      const reduce = records?.reduce((obj: any, item: any) => {
-        if (!obj[item.date] === item.date) {
-          obj[item.date] = { item };
-        } else {
-          obj[item.date] = { item };
-        }
-        return obj;
-      }, []);
-      console.log(reduce);
-      setRecords(records);
+      setRecords(groupBy(records, 'date'));
     });
   };
 
@@ -117,7 +119,7 @@ export const useGoogle = ({
     };
 
     if (isScriptLoaded) {
-      gapi.load("client:auth2", initClient);
+      gapi.load('client:auth2', initClient);
     }
   }, [clientId, apiKey, discoveryDocs, scope, isScriptLoaded]);
 
@@ -158,6 +160,6 @@ export const useGoogle = ({
     isInitialized,
     isSignedIn,
     projects,
-    records
+    records,
   };
 };

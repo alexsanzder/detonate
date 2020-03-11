9edb5b3053e169773d204b08768d1a375f2579a1
Alejandro Sánchez <alex.sanzb@gmail.com>
2020-03-11 12:26:16 +0100

    Update Login component 🔑

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,62 +1,108 @@
 import * as React from "react";
-import styled from "styled-components";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
-import {
-  Content,
-  FlexboxGrid,
-  Panel,
-  Form,
-  FormGroup,
-  Button,
-  Icon,
-  ControlLabel,
-  FormControl,
-  ButtonToolbar
-} from "rsuite";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 import GoogleAuthContext from "../../contexts/useGoogleAuth";
 
-const Style = styled.div`
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-  align-items: center;
-  .login_box_buttons {
-    display: flex;
-  }
-`;
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Detonate - Time Tracker
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1)
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2)
+    }
+  })
+);
 
 const Login = () => {
   const { handleSignIn } = React.useContext(GoogleAuthContext);
 
+  const classes = useStyles();
+
   return (
-    <Style>
-      <Content>
-        <FlexboxGrid justify="center">
-          <FlexboxGrid.Item colspan={12}>
-            <Panel header={<h3>Login</h3>} bordered>
-              <Form fluid>
-                <FormGroup>
-                  <ControlLabel>Sheet Name</ControlLabel>
-                  <FormControl name="name" />
-                </FormGroup>
-                <FormGroup>
-                  <ControlLabel>Google Sheet ID</ControlLabel>
-                  <FormControl name="sheetId" />
-                </FormGroup>
-                <FormGroup>
-                  <ButtonToolbar>
-                    <Button appearance="primary" onClick={handleSignIn} block>
-                      <Icon icon="google" /> Sign in with Google
-                    </Button>
-                  </ButtonToolbar>
-                </FormGroup>
-              </Form>
-            </Panel>
-          </FlexboxGrid.Item>
-        </FlexboxGrid>
-      </Content>
-    </Style>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="sheedId"
+            label="Google Sheet ID"
+            name="sheedId"
+            autoComplete="sheedId"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="sheedName"
+            label="Google Sheet Name"
+            id="sheedName"
+            autoComplete="sheedName"
+          />
+          <Button
+            fullWidth
+            variant="outlined"
+            size="large"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSignIn}
+          >
+            Sign in with Google
+          </Button>
+          <Grid container></Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 };
 
--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -4,7 +4,7 @@ export const darkTheme = createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "#212121"
+      main: "#2196f3"
     }
   }
 });
